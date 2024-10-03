90d3105352002af794e8ae4c770e4e41863b6aed
aallyn <andrew.allyn@gmail.com>
2024-10-02 22:38:49 -0400

    AA lobster and then covariate work

--- Code/2_ExtractCovariates.R
+++ Code/2_ExtractCovariates.R
@@ -5,6 +5,13 @@ library(sf)
 library(raster)
 library(lubridate)
 library(gmRi)
+source("Code/enhance_r_funcs.R")
+
+file_path_sans_ext <- function(x, compression = FALSE) {
+  if (compression)
+    x <- sub("[.](gz|bz2|xz)$", "", x)
+  sub("([^.]+.+)\\.[[:alnum:]]+$", "\\1", x)
+}
 
 points_to_sf<- function(points){
   sf_out<- st_as_sf(points, coords = c("DECDEG_BEGLON", "DECDEG_BEGLAT"), crs = 4326, remove = FALSE)
@@ -71,3 +78,30 @@ all_tows_with_all_covs %>%
   
 
 unique(all_tows_with_all_covs$EST_YEAR[is.na(all_tows_with_all_covs$BT_seasonal) == T])
+
+#####
+## Dynamic covariates -- AA
+#####
+# It looks like the above is working, we just need to figure out what we want to do with the updated dataset bits. For now and to get things rolling, maybe we just chop to 1985-2020?
+
+# For what it is worth, here's if you wanted to do more than just BT...I hope? I really need to update the documentation on this stuff!
+
+# After making sure all of the raster stacks are in the "dynamic" folder and deleting the defunct ".dvc" nonsense. Also seeing the SST.gri file never uploaded to Box...of course!
+dynamic_dir<- paste0(proj_box_path, "covariates/dynamic")
+dynamic_stacks <- dynamic_covariates_read(dynamic_dir)
+
+# Run dynamic_2d_extract_wrapper function
+all_tows_with_all_covs <- dynamic_2d_extract_wrapper(dynamic_covariates_list = dynamic_stacks, t_summ = "seasonal", t_position = NULL, sf_points = all_tows_with_static_covs, date_col_name = "DATE", df_sf = "df", out_dir = "Data/Derived/")
+
+# Check it out
+summary(all_tows_with_all_covs)
+
+unique(all_tows_with_all_covs$EST_YEAR[is.na(all_tows_with_all_covs$SST_seasonal) == T])
+
+# Not entirely clear to me why we are missing some during the "good" years...potentially insure points?
+t <- all_tows_with_all_covs[which(is.na(all_tows_with_all_covs$SST_seasonal) == T), ] |>
+  filter(EST_YEAR >= 1985 & EST_YEAR <= 2019) |>
+  distinct()
+
+plot(t$DECDEG_BEGLON, t$DECDEG_BEGLAT) # Ahhhh fun :)
+
